ff19839ab04a752a47692e8164981fec4faabcc3
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-04-15 21:36:29 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,12 +1,19 @@
+import java.util.scanner
 class positive
-public static void main(string arg[])
+ public static void main(string arg[])
 {
-if(7<3)
-{
-system.out.print("positive")
+  int number;
+  sc=new scanner(system.in);
+  system.out.print("enter the any integer value");
+  number=sc.nextint();
+  if(number>=0)
+  {
+    system.out.print("enter the positive number");
+  }
+  else
+  {
+    system.out.print("enterv the negative number");
+  }
 }
-else
-{
-system.out.print("negative")
-}
-}
+
+    
