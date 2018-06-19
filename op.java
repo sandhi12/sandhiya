7e34c84270514cf6a4de128532493b8c38d22f36
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-06-19 11:09:38 +0530

    Create op.java

---
+++ op.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class op
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%2==0)
+{
+System.out.print("odd");
+}
+else
+{
+System.out.print("even");
+}
+}
+}
