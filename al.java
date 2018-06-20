2b0810b556b3ef941e56cb4f1d76fc38d4ad7999
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-06-20 21:38:11 +0530

    Create al.java

---
+++ al.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+class al
+{
+public static void main(String args[])
+{
+boolean isVowel=false;
+Scanner sc=new Scanner(System.in);
+char ch=sc.next().charAt(0);
+switch(ch)
+{
+case 'a':
+case 'e':
+case 'i':
+case 'o':
+case 'u';
+case 'A':
+case 'E':
+case 'I':
+case 'O':
+case 'U':
+}
+if(isVowe==true)
+{
+System.out.print("vowel");
+}
+else
+{
+System.out.print("consonant");
+}
+}
+}
