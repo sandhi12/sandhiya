8611d3b12583c42d71a1128a82aaf09290040641
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-06-20 21:21:04 +0530

    Update and rename alp.java to alpha.java

---
+++ alpha.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+class alpha
+{
+    public static void main(String args[])
+    {
+  
+    boolean isVowel=false;;
+	Scanner scanner=new Scanner(System.in);
+
+	char ch=scanner.next().charAt(0); 
+	
+	switch(ch)
+	{
+	   case 'a' :
+	   case 'e' :
+    	   case 'i' :
+	   case 'o' :
+	   case 'u' :
+	   case 'A' :
+	   case 'E' :
+	   case 'I' :
+	   case 'O' :
+	   case 'U' : //isVowel = true;
+	}
+	if(isVowel == true) {
+	   System.out.println("vowel");
+	}
+	else {
+	   
+		System.out.println("consonant");
+	  
+        }
+   }
+}
+
+
+
