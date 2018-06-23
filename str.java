2157f719d1bdaa68f3281a40b7189323b8988462
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-06-23 10:16:59 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class str
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        //String s="Hello";
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        System.out.println("Hello");
+        
+        
+    }
+    }
