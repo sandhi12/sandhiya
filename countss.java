347556208b02db9bd71fd8d2acdfc160149d2d1d
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-06-25 21:47:39 +0530

    Create countss.java

---
+++ countss.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class countss
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int count=0;
+        while(a!=0)
+        {
+        a=a/10;
+        count++;
+        }
+        
+        System.out.println(count);
+        
+        
+        
+        
+    }
+}
+
+
