cf0efeafe432ac426f317626665925bfeb24041d
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-06-30 09:47:05 +0530

    Create arr.java

---
+++ arr.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class arr
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double c=Math.pow(a,b);
+        System.out.print(c);
+        
+        
+        
+        
+    }
+}
+
