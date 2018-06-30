e13dac740ef0439e4613fd6cea689434335a8440
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-06-30 11:51:42 +0530

    Create pri.java

---
+++ pri.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class pri
+{
+    public static void main(String args[])
+    {
+        //float avg=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.print("NotPrime");
+        }
+        else
+        {
+            System.out.print("Prime");
+        }
+    }
+}
+
+
