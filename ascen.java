cff3090a9b6007f7a1d9e4b0b8a50da181fcd64d
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-07-01 21:33:11 +0530

    Create ascen.java

---
+++ ascen.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+class ascen
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.print(a[0]);
+        
+       
+        
+    } 
+	
+}
+
+
+
+
+
+
+
+
+
