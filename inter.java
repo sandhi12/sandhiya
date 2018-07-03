9491a43f8136bc66bcf15bded99c38a2af163644
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-07-03 21:11:06 +0530

    Create inter.java

---
+++ inter.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+class inter
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        //String s=sc.nextLine();
+        int a=sc.next().charAt(0);
+        if(a>='0'&&a<='9')
+        {
+            System.out.print("Yes");
+        }
+        else
+        {
+            System.out.print("No");
+        }
+    } 
+	
+}
+
+
+
+
+
+
+
