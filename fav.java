0871c9a951e84a9a8a2bb168be5762de422c4b3a
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-07-04 21:51:35 +0530

    Rename fa.java to fav.java

---
+++ fav.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class aes
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.print(fact);
+        
+    }
+}
+
