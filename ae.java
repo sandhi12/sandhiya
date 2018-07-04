5cc5145a5344661befe589ed82afd34aad6c7f5a
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-07-04 22:14:26 +0530

    Create ae.java

---
+++ ae.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ae
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        StringBuffer sb=new StringBuffer(s);
+        System.out.print(sb.reverse());
+        
+    }
+}
+
+
