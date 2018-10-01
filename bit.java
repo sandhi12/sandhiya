21aaf6170647b50b51aa2cac12dc371e4dd4d127
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2018-10-01 12:20:39 +0530

    Create bit.java

---
+++ bit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class bit
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.print(a+" "+b+" ");
+            
+    }
+}
+
+
+
